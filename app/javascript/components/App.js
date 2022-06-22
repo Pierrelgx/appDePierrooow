cd4b6bd043c9e8c8759bf45fb28bfd42700c86e7
Pierre Legoux <pierrelegoux@MacBook-Pro-de-Pierre.local>
2022-06-22 17:16:04 +0200

    react and rails branched

---
+++ app/javascript/components/App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class App extends Component {
+    render() {
+        return(
+            <h1> Hello Aurél, holà Niall ! C'est votre application qui vous parle... Ecoutez la voix. </h1>
+        )
+    }
+}
+
+export default App 
